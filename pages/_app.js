0d1adec4873a9b89fde7d2390b5c649059652178
JUZKEN <mferro.acc@gmail.com>
2022-06-01 13:09:43 +0100

    added custom cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import React from "react";
 import '../styles/globals.scss'
 import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { linkResolver, repositoryName } from '../prismicio'
+import dynamic from 'next/dynamic'
+
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,9 +20,14 @@ export default function App({ Component, pageProps }) {
             {children}
           </a>
         </Link>
-      )}
-    >
+      )}>
       <PrismicPreview repositoryName={repositoryName}>
+        <AnimatedCursor 
+          innerSize={8}
+          outerScale={7}
+          outerAlpha={0.1}
+          color='255, 255, 255'
+          />
         <Component {...pageProps} />
       </PrismicPreview>
     </PrismicProvider>

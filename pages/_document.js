ef5e82550acbc4ea7df388469458320d264c3e61
Miguel Vieira <miguel.vieira@indyaner.ch>
2022-06-01 14:40:06 +0100

    added favicon + meta tahgs

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,16 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="/fonts/stylesheet.css" rel="stylesheet"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#232025"/>
+          <meta name="msapplication-TileColor" content="#232025"/>
+          <meta name="theme-color" content="#232025"></meta>
+          <title>Miguel Ferro - Web Portfolio</title>
+          <meta name="description" content="Hi. I am Miguel. I am a 21 years old passionate UI/UX Designer & Web Developer. Be sure to look at some of the projects I've worked on over the past years!"></meta>
+          <meta name="author" content="Miguel Ferro"></meta>
         </Head>
         <body>
           <Main />

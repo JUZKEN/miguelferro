692c1ee1061f793db6e528a8b4fff7a4f11fdad8
JUZKEN <mferro.acc@gmail.com>
2022-06-01 15:37:19 +0100

    added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,19 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#232025"/>
           <meta name="msapplication-TileColor" content="#232025"/>
           <meta name="theme-color" content="#232025"></meta>
+          <meta name="title" content="Miguel Ferro - Web Portfolio"></meta>
           <meta name="description" content="Hi. I am Miguel. I am a 21 years old passionate UI/UX Designer & Web Developer. Be sure to look at some of the projects I've worked on over the past years!"></meta>
           <meta name="author" content="Miguel Ferro"></meta>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://www.miguelferro.com/"/>
+          <meta property="og:title" content="Miguel Ferro - Web Portfolio"/>
+          <meta property="og:description" content="Hi. I am Miguel. I am a 21 years old passionate UI/UX Designer & Web Developer. Be sure to look at some of the projects I've worked on over the past years!"/>
+          <meta property="og:image" content="/banner.jpg"/>
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://www.miguelferro.com/"/>
+          <meta property="twitter:title" content="Miguel Ferro - Web Portfolio"/>
+          <meta property="twitter:description" content="Hi. I am Miguel. I am a 21 years old passionate UI/UX Designer & Web Developer. Be sure to look at some of the projects I've worked on over the past years!"/>
+          <meta property="twitter:image" content="/banner.jpg"></meta>
         </Head>
         <body>
           <Main />
